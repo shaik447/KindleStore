dcba58fe0a09af962960273fb91cd4bd0e65f3eb
shaik447 <shaik.imp@gmail.com>
2017-06-04 22:34:03 +0300

    Initial Commit

---
+++ KindleStore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KindleStore
+//
+//  Created by shaik mulla syed on 6/4/17.
+//  Copyright © 2017 shaik mulla syed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
